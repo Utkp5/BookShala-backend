2d26515e2f1d5dd0113e679c400c46de5e205f1f
Utkp5 <utkp200@gmail.com>
2023-01-17 20:48:25 +0530

    bookshala backend

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const cors = require('cors');
 const logger = require("morgan");
 
 
-// https://book-shala-backend.vercel.app/
 
 //viewengine
 app.set("view engine", "ejs");
@@ -13,7 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin: true, credentials: true}));
 app.use(logger("dev"));
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: false }));
